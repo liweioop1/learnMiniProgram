88a2b1d5eb33c8e838309e6a29260da348364242
可爱玩具熊 <978713427@qq.com>
2020-03-08 21:10:09 +0800

    保存1

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,37 +1,12 @@
-// pages/home/home.js
+// pages/profile/profile.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    name:'coderwhy',
-    age:18,
-    students:[
-      {id:110,name:'kobe',age:30},
-      { id: 111, name: 'james', age: 28 }, 
-      { id: 112, name: 'curry', age: 32 },
-      { id: 113, name: 'why', age: 18 },
-    ],
-    counter:0
+
   },
-  handleBtnClick(){
-    this.setData({
-      counter:this.data.counter + 1
-    })
-  },
-  handeSubaction(){
-    this.setData({
-      counter: this.data.counter - 1
-    })
-  },
-
-
-
-
-
-
-
 
   /**
    * 生命周期函数--监听页面加载
